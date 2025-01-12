41fcc8fceadcbc403acc81c5f7e23c3b7752a78b
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2025-01-13 00:16:40 +0100

    [d3d9] Set color space for swap chain image

--- src/d3d9/d3d9_swapchain.cpp
+++ src/d3d9/d3d9_swapchain.cpp
@@ -869,6 +869,14 @@ namespace dxvk {
         cHud            = m_hud,
         cFrameId        = m_wctx->frameId
       ] (DxvkContext* ctx) {
+        // Update back buffer color space as necessary
+        if (cSrcView->image()->info().colorSpace != cColorSpace) {
+          DxvkImageUsageInfo usage = { };
+          usage.colorSpace = cColorSpace;
+
+          ctx->ensureImageCompatibility(cSrcView->image(), usage);
+        }
+
         // Blit back buffer onto Vulkan swap chain
         auto contextObjects = ctx->beginExternalRendering();
 
